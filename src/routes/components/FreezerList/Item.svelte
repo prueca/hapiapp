031a098612cec64117147367acd0f7ba76f0e600
Peter John Rueca <peter.rueca@safetychampion.online>
2026-06-18 19:54:17 +0800

    style: update freezer ui

--- src/routes/components/FreezerList/Item.svelte
+++ src/routes/components/FreezerList/Item.svelte
@@ -5,20 +5,27 @@
 </script>
 
 <div class="freezer">
-    <div class="flex items-center justify-between">
-        <div>{props.id}</div>
-        <button class="ellipsis-btn btn p-0 btn-link btn-sm">
-            <Icon icon="octicon:ellipsis-16" width="20" />
-        </button>
+    <div class="section">
+        <div class="flex items-center justify-between">
+            <div>{props.id}</div>
+            <button class="ellipsis-btn btn p-0 btn-link btn-sm">
+                <Icon icon="octicon:ellipsis-16" width="20" />
+            </button>
+        </div>
+        <div>{props.name}</div>
     </div>
-    <div class="freezer-name">{props.name}</div>
-    <div class="data">
-        <div class="lbl">Cabcon</div>
-        <div class="val">{props.cabcon}</div>
+    <div class="section">
+        <div class="data">
+            <div class="lbl">Cabcon</div>
+            <div class="val">{props.cabcon}</div>
+        </div>
+        <div class="data">
+            <div class="lbl">Assigned On</div>
+            <div class="val">{props.dateAssigned}</div>
+        </div>
     </div>
-    <div class="data">
-        <div class="lbl">Assigned On</div>
-        <div class="val">{props.dateAssigned}</div>
+    <div class="section actions">
+        <button class="btn btn-sm">Submit</button>
     </div>
 </div>
 
@@ -26,10 +33,18 @@
     @reference 'tailwindcss';
 
     .freezer {
-        @apply mb-4 rounded-lg border border-slate-200 bg-slate-50 px-4 py-2;
+        @apply mb-4 overflow-hidden rounded-lg border border-gray-200;
     }
-    .freezer-name {
-        @apply mb-4;
+    .section {
+        @apply px-4 py-2;
+
+        &.actions {
+            @apply mt-2 mb-0 flex items-center justify-between gap-2 bg-gray-50 px-4 py-3;
+
+            .btn {
+                @apply rounded-lg border-none bg-(--c2) text-white;
+            }
+        }
     }
     .data {
         @apply mb-1 flex items-center justify-between gap-1;
