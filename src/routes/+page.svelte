92f5e073c3bffcbe5c11fedfb925a9d16ba17daa
Peter John Rueca <peter.rueca@safetychampion.online>
2026-07-08 18:36:46 +0800

    remove versioning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,16 @@
     import Dock from './components/Dock.svelte'
     import TopBar from './components/TopBar'
     import MerchantName from './components/MerchantName.svelte'
-    import ScanButton from './components/ScanButton.svelte'
     import TabbedContent from './components/TabbedContent.svelte'
+    import ScanButton from './components/ScanButton.svelte'
 </script>
 
 <div>
     <TopBar />
     <MerchantName />
-    <ScanButton />
     <TabbedContent />
     <Dock />
+    <ScanButton />
 </div>
 
 <style lang="postcss">
