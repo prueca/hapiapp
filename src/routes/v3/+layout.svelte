580a3bde8b84a2030aa6aa8c7678b55958c36d94
Lowell Rueca <lowellrrueca@gmail.com>
2026-07-12 21:11:56 +0800

    Add TopBar to v3 layout

--- src/routes/v3/+layout.svelte
+++ src/routes/v3/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+    import TopBar from './components/TopBar'
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<header>
+    <TopBar/>
+</header>
 {@render children()}
