e5f69e96db75e65f54214201112538c8ad984e60
Peter John Rueca <peter.rueca@safetychampion.online>
2026-08-24 23:38:47 +0800

    Revert "create util for extracting and validating payload"
    This reverts commit 62c74a786efb9a0056febaee115df74bde74def9.

--- src/routes/api/accounts/+server.ts
+++ src/routes/api/accounts/+server.ts
@@ -2,7 +2,6 @@ import '$lib/db'
 import { json, error } from '@sveltejs/kit'
 import { StatusCodes, ReasonPhrases } from 'http-status-codes'
 import accountTypes from '$lib/config/account.types'
-import payload from '$lib/payload'
 import z from 'zod'
 import _ from 'lodash'
 
@@ -107,7 +106,14 @@ const fetch = async (account: SubjectAccount, children = false) => {
 }
 
 export const POST = async ({ locals, request }) => {
-    const { account, children } = await payload(request, schema)
+    const payload = await request.json()
+    const validation = schema.safeParse(payload)
+
+    if (!validation.success) {
+        error(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST)
+    }
+
+    const { account, children } = validation.data
     const ok = await verifyAccess(locals, account)
 
     if (!ok) {
