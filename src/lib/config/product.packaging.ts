53e402ffe4ea73c59c43befdc0972b4b7fda3ed6
Lowell Rueca <lowellrrueca@gmail.com>
2026-08-25 20:22:32 +0800

    feat: add product packaging constants

---
+++ src/lib/config/product.packaging.ts
@@ -0,0 +1,17 @@
+const BOX = 'Box'
+const CONE = 'Cone'
+const CUP = 'Cup'
+const GALLON = 'Gallon'
+const PINT = 'Pint'
+const STICK = 'Stick'
+const TUB = 'Tub'
+
+export default {
+    BOX,
+    CONE,
+    CUP,
+    GALLON,
+    PINT,
+    STICK,
+    TUB
+} as const
