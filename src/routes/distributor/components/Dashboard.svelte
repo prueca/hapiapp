e653f5926d923c5a820841fa1560f25237715ad1
Peter John Rueca <peter.rueca@safetychampion.online>
2026-08-27 23:51:08 +0800

    replace dashboard icons

--- src/routes/distributor/components/Dashboard.svelte
+++ src/routes/distributor/components/Dashboard.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-    import accountIcon from '$lib/assets/icons/passport-svgrepo-com.svg'
-    import userIcon from '$lib/assets/icons/users-group-two-rounded-svgrepo-com.svg'
-    import freezerIcon from '$lib/assets/icons/fridge-svgrepo-com.svg'
-    import deploymentIcon from '$lib/assets/icons/truck-fast-svgrepo-com.svg'
-    import cabconIcon from '$lib/assets/icons/cardholder-svgrepo-com.svg'
-    import productIcon from '$lib/assets/icons/notes-svgrepo-com.svg'
-    import orderIcon from '$lib/assets/icons/bag-smile-svgrepo-com.svg'
-    import badOrderIcon from '$lib/assets/icons/bag-cross-svgrepo-com.svg'
+    import Icon from '@iconify/svelte'
 </script>
 
 <div class="dashboard">
@@ -16,51 +9,51 @@
     </h1>
     <div class="dashboard-menu">
         <a href="/distributor/accounts" class="menu-item">
-            <div class="image-wrapper">
-                <img src={accountIcon} alt="" width="32" height="32" />
-            </div>
+            <span class="text-(--c1)">
+                <Icon icon="bi:passport-fill" width="42" />
+            </span>
             <div class="item-label">Accounts</div>
         </a>
         <a href="/distributor/users" class="menu-item">
-            <div class="image-wrapper">
-                <img src={userIcon} alt="" width="32" height="32" />
-            </div>
+            <span class="text-(--c1)">
+                <Icon icon="flowbite:users-group-solid" width="42" />
+            </span>
             <div class="item-label">Users</div>
         </a>
         <a href="/distributor/freezer" class="menu-item">
-            <div class="image-wrapper">
-                <img src={freezerIcon} alt="" width="32" height="32" />
-            </div>
+            <span class="text-(--c1)">
+                <Icon icon="reicon:fridge-filled" width="42" />
+            </span>
             <div class="item-label">Freezers</div>
         </a>
         <a href="/distributor/freezer" class="menu-item">
-            <div class="image-wrapper">
-                <img src={deploymentIcon} alt="" width="32" height="32" />
-            </div>
+            <span class="text-(--c1)">
+                <Icon icon="reicon:truck-fast-filled" width="42" />
+            </span>
             <div class="item-label">Deployments</div>
         </a>
         <a href="/distributor/cabcon" class="menu-item">
-            <div class="image-wrapper">
-                <img src={cabconIcon} alt="" width="32" height="32" />
-            </div>
+            <span class="text-(--c1)">
+                <Icon icon="reicon:cardholder-filled" width="42" />
+            </span>
             <div class="item-label">Cabcon</div>
         </a>
         <a href="/distributor/products" class="menu-item">
-            <div class="image-wrapper">
-                <img src={productIcon} alt="" width="32" height="32" />
-            </div>
+            <span class="text-(--c1)">
+                <Icon icon="reicon:notes-filled" width="42" />
+            </span>
             <div class="item-label">Products</div>
         </a>
         <a href="/distributor/orders" class="menu-item">
-            <div class="image-wrapper">
-                <img src={orderIcon} alt="" width="32" height="32" />
-            </div>
+            <span class="text-(--c1)">
+                <Icon icon="reicon:bag-smile-filled" width="42" />
+            </span>
             <div class="item-label">Orders</div>
         </a>
         <a href="/distributor/bad-orders" class="menu-item">
-            <div class="image-wrapper">
-                <img src={badOrderIcon} alt="" width="32" height="32" />
-            </div>
+            <span class="text-(--c1)">
+                <Icon icon="solar:bag-cross-bold" width="42" />
+            </span>
             <div class="item-label">Bad Orders</div>
         </a>
     </div>
@@ -83,14 +76,6 @@
 
     .menu-item {
         @apply flex flex-col items-center text-center;
-
-        .image-wrapper {
-            @apply rounded-full bg-gray-100 p-3;
-        }
-
-        img {
-            filter: sepia(1) saturate(25) hue-rotate(360deg);
-        }
     }
 
     .item-label {
