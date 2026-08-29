1db5a02832e9c01cc837018703c6bd6fcd5312bf
Peter John Rueca <peter.rueca@safetychampion.online>
2026-08-30 01:59:05 +0800

    set auth context in /dstributor/+layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,8 @@
 <script lang="ts">
     import './layout.css'
     import favicon from '$lib/assets/favicon.svg'
-    import { setContext } from 'svelte'
 
-    let { data, children } = $props()
-
-    setContext<{ user: AuthUser; account: AuthAccount }>('auth', {
-        user: data.user!,
-        account: data.account!
-    })
+    let { children } = $props()
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
