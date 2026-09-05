51629b4c3c69a869cd1fb06c682521f900e993f3
Lowell Rueca <lowellrrueca@gmail.com>
2026-09-05 08:27:04 +0800

    add freezer status constants
    - housed-available, for-deployment, deployed-designated, for-pullout, pullout
    - for-replacement-broken-unit, for-replacement-downgrade, for-replacement-upgrade

---
+++ src/lib/config/freezer.status.ts
@@ -0,0 +1,19 @@
+const HOUSED_AVAILABLE = 'housed - available'
+const FOR_DEPLOYMENT = 'for deployment'
+const DEPLOYED_DESIGNATED = 'deployed - designated'
+const FOR_PULLOUT = 'for pullout'
+const PULLOUT = 'pullout'
+const FOR_REPLACEMENT_BROKEN_UNIT = 'for replacement - broken unit'
+const FOR_REPLACEMENT_DOWNGRADE = 'for replacement - downgrade'
+const FOR_REPLACEMENT_UPGRADE = 'for replacement - upgrade'
+
+export default {
+    HHOUSED_AVAILABLE: HOUSED_AVAILABLE,
+    FOR_DEPLOYMENT,
+    DEPLOYED_DESIGNATED: DEPLOYED_DESIGNATED,
+    FOR_PULLOUT,
+    PULLOUT,
+    FOR_REPLACEMENT_BROKEN_UNIT,
+    FOR_REPLACEMENT_DOWNGRADE,
+    FOR_REPLACEMENT_UPGRADE
+} as const
