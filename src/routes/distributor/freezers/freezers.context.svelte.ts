1504c4f28619ba7ee1e67782bd2be17200a49a90
Lowell Rueca <lowellrrueca@gmail.com>
2026-09-07 22:11:32 +0800

    feat: add create freezer flow to freezers context
    - Expose option lists to drive the create form dropdowns
    - Track create form state, submission and error
    - Post new freezers to the api and invalidate on success

--- src/routes/distributor/freezers/freezers.context.svelte.ts
+++ src/routes/distributor/freezers/freezers.context.svelte.ts
@@ -6,6 +6,9 @@ import {
     type SortKey,
     type StatusFilter
 } from '$lib/types/freezer'
+import { invalidateAll } from '$app/navigation'
+import api from '$lib/api'
+import { modelOptions, capacityOptions, yearModelOptions, brandOptions } from '$lib/config/freezer.options'
 import _ from 'lodash'
 
 class Freezers {
@@ -20,8 +23,22 @@ class Freezers {
 
     openFilters = $state(false)
 
+    openCreate = $state(false)
+    submitting = $state(false)
+    error = $state('')
+
+    newModel = $state('')
+    newCapacity = $state('')
+    newBrand = $state('')
+    newYearModel = $state('')
+    newBarcode = $state('')
+
     sortOptions = SORT_OPTIONS
     statusFilterOptions = STATUS_FILTER_OPTIONS
+     modelOptions = modelOptions as unknown as string[]
+     capacityOptions = capacityOptions as unknown as number[]
+     yearModelOptions = yearModelOptions as unknown as number[]
+     brandOptions = brandOptions as unknown as string[]
 
     filtered = $derived(
         _.chain(this.items)
@@ -72,6 +89,55 @@ class Freezers {
     load(items: FreezerType[]) {
         this.items = items
         this.resetVisibleCount()
+    }
+
+    closeCreate() {
+        this.openCreate = false
+    }
+
+    resetCreateForm() {
+        this.newModel = ''
+        this.newCapacity = ''
+        this.newBrand = ''
+        this.newYearModel = ''
+        this.newBarcode = ''
+    }
+
+    async submitCreate() {
+        this.submitting = true
+        this.error = ''
+
+        try {
+            const res = await api.post('freezers', {
+                json: {
+                    model: this.newModel,
+                    capacity: Number(this.newCapacity),
+                    brand: this.newBrand,
+                    yearModel: Number(this.newYearModel),
+                    barcode: this.newBarcode
+                }
+            })
+
+            await res.json()
+
+            this.closeCreate()
+            this.resetCreateForm()
+
+            await invalidateAll()
+        } catch (e: any) {
+            const data = e?.data
+
+            const message: string | null =
+                typeof data === 'object' && data !== null && typeof data.message === 'string'
+                    ? data.message
+                    : typeof data === 'string'
+                      ? data
+                      : e?.response?.statusText || e?.message || null
+
+            this.error = message || 'Something went wrong while creating the freezer.'
+        } finally {
+            this.submitting = false
+        }
     }
 }
 
