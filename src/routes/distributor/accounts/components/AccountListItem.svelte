be3366fbb24b7abfeebaa6f755cc37eb2b3f8c61
Peter John Rueca <peter.rueca@safetychampion.online>
2026-09-14 23:20:34 +0800

    place action buttons in a dropdown

--- src/routes/distributor/accounts/components/AccountListItem.svelte
+++ src/routes/distributor/accounts/components/AccountListItem.svelte
@@ -8,26 +8,36 @@
 </script>
 
 <div class="account-list-item">
-    <div class="flex items-center justify-between gap-4">
-        <div>
-            <div class="mb-1 font-medium">
-                {item.name}
-            </div>
-            <div class="mb-1 text-xs text-gray-400">
-                <span>{_.upperFirst(item.type)}</span>
-                <span class="mx-1">•</span>
-                <span>{item.address}</span>
-            </div>
+    <div class="flex items-center justify-between">
+        <div class="mb-2 font-medium">
+            {item.name}
         </div>
-        <div class="flex items-center gap-2 text-gray-400">
-            <!-- <Icon icon="basil:caret-right-solid" /> -->
-            <button class="btn btn-circle" onclick={() => update.select(item.id)}>
-                <Icon icon="ci:note-edit" width="24" />
+        <div class="dropdown dropdown-end">
+            <button class="btn p-0 btn-link btn-sm">
+                <span class="text-gray-500">
+                    <Icon icon="octicon:ellipsis-16" width="20" />
+                </span>
             </button>
-            <button class="btn btn-circle" onclick={() => del.confirm(item.id)}>
-                <Icon icon="mdi:delete-outline" width="24" />
-            </button>
+            <ul class="dropdown-content menu bg-base-100">
+                <li>
+                    <button onclick={() => update.select(item.id)}>
+                        <Icon icon="ci:note-edit" width="18" />
+                        <span class="ml-1">Update</span>
+                    </button>
+                </li>
+                <li>
+                    <button onclick={() => del.confirm(item.id)}>
+                        <Icon icon="mdi:delete-outline" width="18" />
+                        <span class="ml-1">Delete</span>
+                    </button>
+                </li>
+            </ul>
         </div>
+    </div>
+    <div class="mb-1 text-xs text-gray-400">
+        <span>{_.upperFirst(item.type)}</span>
+        <span class="mx-1">•</span>
+        <span>{item.address}</span>
     </div>
 </div>
 
@@ -36,5 +46,13 @@
 
     .account-list-item {
         @apply not-last:mb-4 not-last:border-b not-last:border-b-gray-100 not-last:pb-4;
+    }
+
+    .dropdown-content {
+        @apply z-1 w-52 rounded-lg p-2 shadow-sm;
+    }
+
+    .menu li > button:active {
+        @apply bg-(--c4) text-(--font-color);
     }
 </style>
