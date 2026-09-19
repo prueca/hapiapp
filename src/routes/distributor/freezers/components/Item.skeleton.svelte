fb4254f61b277f4aaaf05ee4955cdf13d6590bec
Lowell Rueca <lowellrrueca@gmail.com>
2026-09-19 12:30:07 +0800

    remove status badge from freezer skeleton
    Delete the status badge placeholder and its associated styles.

--- src/routes/distributor/freezers/components/Item.skeleton.svelte
+++ src/routes/distributor/freezers/components/Item.skeleton.svelte
@@ -1,8 +1,4 @@
 <div class="freezer-item">
-    <div class="top-row">
-        <span class="badge block"></span>
-    </div>
-
     <span class="barcode block w-1/2"></span>
     <span class="model block w-2/3"></span>
     <span class="meta block w-1/3"></span>
@@ -15,16 +11,8 @@
         @apply not-last:mb-4 not-last:border-b not-last:border-b-gray-100 not-last:pb-4;
     }
 
-    .top-row {
-        @apply flex justify-end;
-    }
-
     .block {
         @apply my-1 rounded bg-gray-100;
-    }
-
-    .badge {
-        @apply my-0 h-5 w-16 rounded-full;
     }
 
     .barcode {
