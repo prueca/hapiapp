6f457c39136279a733b3ac0e2b382021b676c92f
Lowell Rueca <lowellrrueca@gmail.com>
2026-09-19 12:31:09 +0800

    add deployments toolbar
    Search field plus filters trigger for the deployments page.

---
+++ src/routes/distributor/deployments/components/Toolbar.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte'
+    import state from '../deployments.context.svelte'
+    import Filters from './Filters.svelte'
+</script>
+
+<div class="border-b border-b-gray-100 p-4">
+      <div class="flex gap-2">
+           <div class="grow">
+                <label class="input w-full rounded-l-md">
+                     <Icon icon="bytesize:search" width="20" />
+                     <input
+                          type="search"
+                          class="grow"
+                          placeholder="Search deployments..."
+                          bind:value={state.query}
+                     />
+                </label>
+           </div>
+
+           <button type="button" class="btn rounded-md" onclick={() => (state.openFilters = true)}>
+                <Icon icon="bi:filter" width="24" />
+           </button>
+      </div>
+</div>
+
+<Filters />
+
+<style lang="postcss">
+     @reference 'tailwindcss';
+</style>
