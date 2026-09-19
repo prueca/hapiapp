a5232aae4df18f03ff04ccf33eac674dcfce807f
Lowell Rueca <lowellrrueca@gmail.com>
2026-09-19 12:29:59 +0800

    point dashboard menu to deployments
    Replace the broken /distributor/freezer link with /distributor/deployments and fix the freezers menu item indentation.

--- src/routes/distributor/components/Dashboard.svelte
+++ src/routes/distributor/components/Dashboard.svelte
@@ -20,13 +20,13 @@
             </span>
             <div class="item-label">Users</div>
         </a>
-         <a href="/distributor/freezers" class="menu-item">
-             <span class="text-(--c1)">
-                 <Icon icon="reicon:fridge-filled" width="42" />
-             </span>
-             <div class="item-label">Freezers</div>
+        <a href="/distributor/freezers" class="menu-item">
+            <span class="text-(--c1)">
+                <Icon icon="reicon:fridge-filled" width="42" />
+            </span>
+            <div class="item-label">Freezers</div>
         </a>
-        <a href="/distributor/freezer" class="menu-item">
+        <a href="/distributor/deployments" class="menu-item">
             <span class="text-(--c1)">
                 <Icon icon="reicon:truck-fast-filled" width="42" />
             </span>
